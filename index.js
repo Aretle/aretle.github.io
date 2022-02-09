d862ae869464d44b6d65bcb4332d0b29798a9a6f
Aretle <dfel0001@student.monash.edu>
2022-02-09 21:33:45 +1100

    add email api credentials

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var website_video = document.getElementById("website_video")
 
-document.getElementById("website_info").onclick = function () {
+document.getElementById("website_info").onclick = function() {
   console.log("More info button clicked");
   website_video.style.display = "inline";
 }
@@ -9,15 +9,15 @@ website_video.addEventListener("click", function() {
   website_video.style.display = "none";
 });
 
-function sendEmail(){
+function sendEmail() {
   Email.send({
-      Host : "smtp.gmail.com",
-      Username : "danielfeltham98@gmail.com",
-      Password : "",
-      To : 'danielfeltham98@gmail.com',
-      From : document.getElementById("contact_email").value,
-      Subject : "Website Contact Form Enquiry",
-      Body : "And this is the body"
+    SecureToken: "6715f5a5-7173-4b07-b6b0-7b0de44da2f9",
+    To: 'danielfeltham98@gmail.com',
+    From: "danielfeltham98@gmail.com",
+    Subject: "Website Contact Form Enquiry",
+    Body: "Name: " + document.getElementById("contact_name").value +
+      "<br> Email: " + document.getElementById("contact_email").value +
+      "<br> Message: " + document.getElementById("contact_message").value
   }).then(
     message => alert(message)
   );
